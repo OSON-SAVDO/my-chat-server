be151a8122a80d8594fdf40ced52dadf332ecbb4
OSON-SAVDO <hadiatulloabdulhajrov@gmail.com>
2026-01-29 12:52:15 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,56 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, { cors: { origin: "*" } });
+const io = require('socket.io')(http, {
+  cors: { origin: "*" }
+});
 
-let onlineUsers = {}; // { phone: socketId }
-let registeredUsers = new Set(); // Рӯйхати ҳамаи рақамҳо
-let offlineMessages = {}; // { phone: [messages] } - Паёмҳое, ки дар навбат ҳастанд
+let users = {}; // Рақам : socket.id
 
 io.on('connection', (socket) => {
-    
-    socket.on('register', (phone) => {
-        socket.phone = phone;
-        onlineUsers[phone] = socket.id;
-        registeredUsers.add(phone);
-        
-        console.log(`Корбар ${phone} онлайн шуд.`);
+  console.log('Пайвастшавии нав: ' + socket.id);
 
-        // Агар барои ин корбар паёмҳои дар навбат истода бошад, мефиристем
-        if (offlineMessages[phone] && offlineMessages[phone].length > 0) {
-            offlineMessages[phone].forEach(msg => {
-                io.to(socket.id).emit('message', msg);
-            });
-            offlineMessages[phone] = []; // Навбатро холӣ мекунем
-        }
-    });
+  // Сабти номи корбар
+  socket.on('register', (phone) => {
+    socket.phone = phone;
+    users[phone] = socket.id;
+    console.log(`Корбар ${phone} онлайн шуд`);
+  });
 
-    // Санҷиши рақам (ҳатто агар Offline бошад)
-    socket.on('check-user', (phone, callback) => {
-        callback(registeredUsers.has(phone));
-    });
+  // Фиристодани паёми матнӣ ё голос
+  socket.on('message', (data) => {
+    const targetSocket = users[data.toPhone];
+    if (targetSocket) {
+      io.to(targetSocket).emit('message', {
+        fromPhone: socket.phone,
+        data: data.data,
+        type: data.type || 'text' // метавонад 'text' ё 'voice' бошад
+      });
+    }
+  });
 
-    socket.on('message', (data) => {
-        const targetSocket = onlineUsers[data.toPhone];
-        const msgPayload = { data: data.data, fromPhone: socket.phone };
+  // Мантиқи зангҳо (WebRTC Signaling)
+  socket.on('call-request', (data) => {
+    const targetSocket = users[data.toPhone];
+    if (targetSocket) {
+      io.to(targetSocket).emit('incoming-call', {
+        fromPhone: socket.phone,
+        type: data.type // 'audio' ё 'video'
+      });
+    }
+  });
 
-        if (targetSocket) {
-            // Агар дӯст онлайн бошад - дарҳол мефиристем
-            io.to(targetSocket).emit('message', msgPayload);
-        } else {
-            // Агар дӯст офлайн бошад - дар сервер захира мекунем
-            if (!offlineMessages[data.toPhone]) offlineMessages[data.toPhone] = [];
-            offlineMessages[data.toPhone].push(msgPayload);
-            console.log(`Паём барои ${data.toPhone} дар сервер ҳифз шуд (Offline).`);
-        }
-    });
-
-    socket.on('disconnect', () => {
-        if (socket.phone) delete onlineUsers[socket.phone];
-    });
+  socket.on('disconnect', () => {
+    for (let phone in users) {
+      if (users[phone] === socket.id) {
+        delete users[phone];
+        break;
+      }
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Сервер дар порти ${PORT} фаъол аст`));
+http.listen(PORT, () => {
+  console.log('Сервер дар порти ' + PORT + ' кор мекунад');
+});
